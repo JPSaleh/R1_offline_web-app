e93733afc366c813a83e3870e8a8a71515465007
JPSaleh <140678665+JPSaleh@users.noreply.github.com>
2025-01-30 11:32:23 -0800

    dependencies.py

--- dependencies.py
+++ dependencies.py
@@ -1,4 +1,14 @@
-Install dependencies:
+Python dependencies:
 pip install fastapi uvicorn
 pip install requests
 pip install fastapi.middleware.cors
+
+# 1️⃣ Create the React app (done initially)
+npx create-react-app client --use-npm
+
+# 2️⃣ Install required dependencies
+cd client
+npm install axios
+npm install react-markdown remark-math rehype-katex
+npm install react-syntax-highlighter
+npm install react-loading
